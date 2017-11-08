e51c2a4018dee2c33a49d771bd4e11acb04316fd
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-11-08 03:26:34 -0800

    add HitBTC/Gemini Interfaces to parse incoming messages

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"golang.org/x/net/websocket"
+	"log"
 	"os"
 )
 
@@ -10,8 +12,8 @@ func initAllConnections() {
 	messages := make(chan []byte)
 	var urls [4]string
 
-	urls[0] = "wss://api.gemini.com/v1/marketdata/btcusd"
-	urls[1] = "ws://api.hitbtc.com:80"
+	urls[0] = "ws://api.hitbtc.com:80"
+	urls[1] = "wss://api.gemini.com/v1/marketdata/btcusd"
 	urls[2] = "wss://api.gemini.com/v1/marketdata/ethbtc"
 	urls[3] = "wss://api.gemini.com/v1/marketdata/ethusd"
 
@@ -27,18 +29,30 @@ func initAllConnections() {
 			defer ws.Close()
 
 			incomingMessages := make(chan string)
-			go readExchangeMessages(ws, incomingMessages)
+			go readExchangeMessages(ws, incomingMessages, u)
+
+			var countNum int64
+			countNum = 0
+
 			for {
 				select {
 				case message := <-incomingMessages:
 					if u == urls[0] {
-						fmt.Println(`Message Received FROM GEMINI BTCUSD:`, message)
+						countNum = countNum + 1
+						fmt.Printf("\n\n\n\nMessage Received FROM HITBTC: %s", message)
+						fmt.Printf("\n\n NUMBER OF Polls ARE: %d \n\n\n", countNum)
 					} else if u == urls[1] {
-						fmt.Println(`Message Received FROM HITBTC:`, message)
+						countNum = countNum + 1
+						fmt.Println(`Message Received FROM GEMINI BTCUSD:`, message)
+						fmt.Printf("\n\n NUMBER OF Polls ARE: %d \n\n\n", countNum)
 					} else if u == urls[2] {
+						countNum = countNum + 1
 						fmt.Println(`Message Received FROM GEMINI ETHBTC:`, message)
+						fmt.Printf("\n\n NUMBER OF Polls ARE: %d \n\n\n", countNum)
 					} else if u == urls[3] {
+						countNum = countNum + 1
 						fmt.Println(`Message Received FROM GEMINI ETHUSD:`, message)
+						fmt.Printf("\n\n NUMBER OF Polls ARE: %d \n\n\n", countNum)
 					}
 				}
 			}
@@ -46,19 +60,85 @@ func initAllConnections() {
 	}
 
 	for m := range messages {
-		fmt.Printf("%s\n", m)
+		fmt.Printf("%s\n\n\n\n\n", m)
 	}
 }
 
-func readExchangeMessages(ws *websocket.Conn, incomingMessages chan string) {
+func readExchangeMessages(ws *websocket.Conn, incomingMessages chan string, u string) {
 	for {
+		hitbtcResponse := new(struct {
+			MarketDataIncrementalRefresh struct {
+				SeqNo          int    `json:"seqNo"`
+				Symbol         string `json:"symbol"`
+				ExchangeStatus string `json:"exchangeStatus"`
+				Ask            []struct {
+					Price string `json:"price"`
+					Size  int    `json:"size"`
+				} `json:"ask"`
+				Bid []struct {
+					Price string `json:"price"`
+					Size  int    `json:"size"`
+				} `json:"bid"`
+				Trade []struct {
+					Price     string `json:"price"`
+					Size      int    `json:"size"`
+					TradeID   int    `json:"tradeId"`
+					Timestamp int64  `json:"timestamp"`
+					Side      string `json:"side"`
+				} `json:"trade"`
+				Timestamp int64 `json:"timestamp"`
+			} `json:"MarketDataIncrementalRefresh"`
+		})
+
+		// geminiResponse := new(struct {
+		// 	Type           string `json:"type"`
+		// 	EventID        int64  `json:"eventId"`
+		// 	Timestamp      int    `json:"timestamp"`
+		// 	Timestampms    int64  `json:"timestampms"`
+		// 	SocketSequence int    `json:"socket_sequence"`
+		// 	Events         []struct {
+		// 		Type      string `json:"type"`
+		// 		Side      string `json:"side"`
+		// 		Price     string `json:"price"`
+		// 		Remaining string `json:"remaining"`
+		// 		Delta     string `json:"delta"`
+		// 		Reason    string `json:"reason"`
+		// 	} `json:"events"`
+		// })
+
 		var message string
-		// err := websocket.JSON.Receive(ws, &message)
+		// type jsonMessage struct {
+		// 	Data string `json:"data"`
+		// }
+
+		// var jj jsonMessage
+		// err := websocket.JSON.Receive(ws, &jj)
+
 		err := websocket.Message.Receive(ws, &message)
 		if err != nil {
-			fmt.Printf("Error Is: %s\n", err.Error())
+			fmt.Printf("Error Is: %s\n", err.Error(), err)
 			return
 		}
+
+		if u == "ws://api.hitbtc.com:80" {
+			if err := json.NewDecoder(ws).Decode(hitbtcResponse); err != nil {
+				log.Fatal(err)
+			}
+
+			hitbtcMessage := map[string]interface{}{"symbol": hitbtcResponse.MarketDataIncrementalRefresh.Symbol, "exchangeStatus": hitbtcResponse.MarketDataIncrementalRefresh.ExchangeStatus, "ask": hitbtcResponse.MarketDataIncrementalRefresh.Ask, "bid": hitbtcResponse.MarketDataIncrementalRefresh.Bid, "trade": hitbtcResponse.MarketDataIncrementalRefresh.Trade, "timestamp": hitbtcResponse.MarketDataIncrementalRefresh.Timestamp}
+
+			fmt.Printf("NEW INTERFACE IS: %s", hitbtcMessage)
+		}
+		//  else {
+		// 	if err := json.NewDecoder(ws).Decode(geminiResponse); err != nil {
+		// 		log.Fatal(err)
+		// 	}
+
+		// 	geminiMessage := map[string]interface{}{"type": geminiResponse.Events.Type, "side": geminiResponse.Events.Side, "price": geminiResponse.Events.Price, "timestamp": geminiResponse.Timestamp}
+
+		// 	fmt.Printf("NEW INTERFACE IS: %s", geminiMessage)
+		// }
+
 		incomingMessages <- message
 	}
 }
